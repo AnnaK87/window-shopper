f4e59763bc978ea16d8b6c68026fad7a2b107676
AnnaK87 <anna.kaukh@gmail.com>
2018-11-18 01:23:21 +0400

    calculate btn added

--- window-shopper/window-shopper/MainVC.swift
+++ window-shopper/window-shopper/MainVC.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    
+    @IBOutlet weak var itemTxt: CurrencyTxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calculateBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calculateBtn.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 0.6669252997)
+        calculateBtn.setTitle("Calculate", for: .normal)
+        calculateBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calculateBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calculateBtn
+        itemTxt.inputAccessoryView = calculateBtn
+
     }
 
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    @objc func calculate() {
+        print("its working")
+    }
+ 
 }
 
--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
+@IBDesignable
+
 class CurrencyTxtField: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        customizeTxtField()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeTxtField()
+    }
+ 
+    func customizeTxtField() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2495451627)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -22,7 +32,6 @@ class CurrencyTxtField: UITextField {
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
- 
     
     
 }
