c499d7417a8592bbef965160886861704737fdbb
AnnaK87 <anna.kaukh@gmail.com>
2018-11-17 18:53:46 +0400

    initial commit

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Anna Kaukh on 11/17/18.
+//  Copyright © 2018 Anna Kaukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
