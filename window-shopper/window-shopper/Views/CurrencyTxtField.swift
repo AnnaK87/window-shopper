47f983f3ed61121c4b81d10b0e85bfe7dcc199ed
AnnaK87 <anna.kaukh@gmail.com>
2018-11-18 17:00:19 +0400

    custom drawing of Lbl with drawRect

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -11,7 +11,25 @@ import UIKit
 @IBDesignable
 
 class CurrencyTxtField: UITextField {
-
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height/2 - size/2), width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2495451627)
+        currencyLbl.textAlignment = .center
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        currencyLbl.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = Locale.current
+        
+        
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeTxtField()
     }
@@ -20,11 +38,12 @@ class CurrencyTxtField: UITextField {
         super.awakeFromNib()
         customizeTxtField()
     }
- 
+    
     func customizeTxtField() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2495451627)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let placeHolderText = placeholder {
             let atributedText = NSAttributedString(string: placeHolderText, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
